0ea2315a7f55dca842de23cb8cfb86fde59ad9fa
Eneroth3 <eneroth3@gmail.com>
2019-10-26 21:17:26 +0200

    Add reload method

--- src/ene_viewport_resizer2/main.rb
+++ src/ene_viewport_resizer2/main.rb
@@ -1,7 +1,28 @@
 module Eneroth
   module ViewportResizer2
     Sketchup.require "#{PLUGIN_ROOT}/viewport.rb"
-
     # ...
+
+    # Reload extension.
+    #
+    # @param clear_console [Boolean] Whether console should be cleared.
+    # @param undo [Boolean] Whether last oration should be undone.
+    #
+    # @return [void]
+    def self.reload(clear_console = true, undo = false)
+      # Hide warnings for already defined constants.
+      verbose = $VERBOSE
+      $VERBOSE = nil
+      Dir.glob(File.join(PLUGIN_ROOT, "**/*.{rb,rbe}")).each { |f| load(f) }
+      $VERBOSE = verbose
+
+      # Use a timer to make call to method itself register to console.
+      # Otherwise the user cannot use up arrow to repeat command.
+      UI.start_timer(0) { SKETCHUP_CONSOLE.clear } if clear_console
+
+      Sketchup.undo if undo
+
+      nil
+    end
   end
 end
